0f3bd2b29562e3c5d5c8f07aa6ccd4e207dca778
leiwenpeng <leiwenpeng1994@outlook.com>
2021-07-28 14:28:06 +0800

    feat: commit

--- dev-packages/command-line-tool/dist/CommandManager.d.ts
+++ dev-packages/command-line-tool/dist/CommandManager.d.ts
@@ -1,7 +1,7 @@
 declare class CommandOptions {
-    readonly optionName: string;
-    readonly shortOptionName?: string;
-    readonly description?: string;
+  readonly optionName: string;
+  readonly shortOptionName?: string;
+  readonly description?: string;
 }
 /**
  * @public
@@ -11,22 +11,47 @@ declare class CommandOptions {
  * ```
  */
 export default class CommandManager {
-    /**
-     *
-     * @public
-     * @param subCommandName
-     * @param options
-     *
-     * @example  ```ts
-     * CommandManager.registerSubCommand(subCommandName, [
-     *  {
-     *    optionName: 'run-dry',
-     *    shortOptionName: 'd',
-     *    description: 'Will run command without limitation'
-     *  }
-     * ])
-     * ```
-     */
-    registerSubCommand(subCommandName: string, options: CommandOptions[]): void;
+  _commandName: string;
+  _commandDescription: string;
+
+  constructor({ commandName, commandDescription }) {
+    this._commandName = commandName;
+    this._commandDescription = commandDescription;
+  }
+  /**
+   * @public
+   *
+   * @description 指令名称
+   */
+  get commandName() {
+    return this._commandName;
+  }
+
+  /**
+   * @public
+   *
+   * @description 指令描述
+   */
+  get commandDescription() {
+    return this._commandDescription;
+  }
+
+  /**
+   *
+   * @public
+   * @param subCommandName
+   * @param options
+   *
+   * @example  ```ts
+   * CommandManager.registerSubCommand(subCommandName, [
+   *  {
+   *    optionName: 'run-dry',
+   *    shortOptionName: 'd',
+   *    description: 'Will run command without limitation'
+   *  }
+   * ])
+   * ```
+   */
+  registerSubCommand(subCommandName: string, options: CommandOptions[]): void;
 }
 export {};
--- dev-packages/raydium/src/hooks/BaseHooks.ts
+++
@@ -1,8 +0,0 @@
-import { SyncHook } from 'tapable';
-
-export class BaseHooks {
-  public readonly pre = new SyncHook();
-  public readonly after = new SyncHook();
-}
-
-export class PluginHook extends BaseHooks {}
