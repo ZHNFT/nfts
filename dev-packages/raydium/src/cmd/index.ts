53df89c374c8cfabf912458a7b5bd0aaf28b5173
leiwenpeng <leiwenpeng1994@outlook.com>
2021-07-28 14:28:22 +0800

    feat: commit

---
+++ dev-packages/raydium/src/cmd/index.ts
@@ -0,0 +1,27 @@
+import { CommandManager, TerminalManager } from '@raydium/command-line-tool';
+import { Command, CommandDescription } from '../constants/CommandNames';
+
+
+export class CommandLineParser extends CommandManager {
+  private readonly _terminal: TerminalManager;
+
+  constructor() {
+    super({
+      commandName: Command,
+      commandDescription: CommandDescription
+    });
+
+    this._terminal = new TerminalManager();
+  }
+
+  get terminal(): TerminalManager {
+    return this._terminal;
+  }
+
+  /**
+   * @public
+   *
+   */
+  execute() {
+  }
+}
---
+++ dev-packages/raydium/src/constants/CommandNames.ts
@@ -0,0 +1,2 @@
+export const Command = 'raydium';
+export const CommandDescription = 'dev, build，test，deliver with raydium';
