549fdcc2a9c4ec373bfb19c680568eb0cae640a0
leiwenpeng <leiwenpeng1994@outlook.com>
2022-03-01 18:08:57 +0800

    Changes;

--- apps/gmf/src/plugins/BasePlugin.ts
+++ apps/gmf/src/plugins/BasePlugin.ts
@@ -6,6 +6,5 @@ export interface IBasePluginDefinition {
 export abstract class BasePlugin implements IBasePluginDefinition {
   abstract readonly description: string;
   abstract readonly name: string;
-
   abstract apply<T extends unknown>(args: T): void;
 }
