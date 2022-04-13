f2f857c974a0e1014439a28f9555db774805fd8f
leiwenpeng <leiwenpeng1994@outlook.com>
2022-04-13 13:24:16 +0800

    feat(node-utils-library): :art: add Glob

--- libs/node-utils-library/src/Glob.ts
+++ libs/node-utils-library/src/Glob.ts
@@ -8,8 +8,52 @@
 // src/*.ts    => ^src\/(\w+)\/*(.ts)
 
 export class Glob {
-  public static match(pattern: string, visitor: (fileName: string) => void): string[] {
+  public static globToRegExp(globPattern: string, flags = ''): RegExp {
+    let reStr = '';
+
+    let i = 0;
+
+    while (i < globPattern.length) {
+      const char = globPattern[i];
+
+      switch (char) {
+        default:
+          reStr += char;
+          break;
+      }
+
+      i++;
+    }
+
+    return new RegExp(reStr, flags);
+  }
+
+  private static match(pattern: string): string[] {
     const fileNames: string[] = [];
     return fileNames;
+  }
+
+  /**
+   * 测试给出的字符串是否匹配 glob 字符串
+   * @param globPattern
+   * @param name
+   */
+  public static test(globPattern: string, name: string): boolean {
+    //
+    return true;
+  }
+
+  /**
+   * 同步地读取匹配规则的文件
+   * @param globPattern
+   */
+  public static sync(
+    globPattern: string,
+    { rootDir = process.cwd() }: { rootDir: string }
+  ): string[] {
+    const names: string[] = [];
+    const regExpPattern = Glob.globToRegExp(globPattern);
+
+    return names;
   }
 }
