89bf5f235e898493178e9153d6947bde68a2e75f
leiwenpeng <leiwenpeng1994@outlook.com>
2022-04-20 18:25:16 +0800

    feat: 重构了Hook包的代码，适合更多的使用场景；

--- libs/hook/src/index.ts
+++ libs/hook/src/index.ts
@@ -1,4 +1,24 @@
-export * from './hooks/AsyncHook';
-export * from './hooks/SyncHook';
-export * from './hooks/ManualHook';
-export * from './hooks/WaterfallHook';
+import { Async } from '@nfts/node-utils-library';
+
+export class Hook<HookNames = string, HookCallbackArgs = void> {
+  private readonly _hooks: Map<HookNames, Async.Task<HookCallbackArgs>[]>;
+
+  constructor() {
+    this._hooks = new Map<HookNames, Async.Task<HookCallbackArgs>[]>();
+  }
+
+  public addHook(hookName: HookNames, callback: Async.Task<HookCallbackArgs>): void {
+    const callbacks = this._hooks.get(hookName) ?? [];
+    callbacks.push(callback);
+    this._hooks.set(hookName, callbacks);
+  }
+  public emitHook(hookName: HookNames, args: HookCallbackArgs): Promise<void> {
+    const _hookCallbacks = this._hooks.get(hookName);
+
+    if (_hookCallbacks && _hookCallbacks.length > 0) {
+      return Async.serialize(_hookCallbacks, args);
+    }
+
+    return Promise.resolve();
+  }
+}
