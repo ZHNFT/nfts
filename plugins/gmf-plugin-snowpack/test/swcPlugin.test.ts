77896da505353839342d8b5b7a0db8d8cad7551f
leiwenpeng <leiwenpeng1994@outlook.com>
2022-04-21 10:40:57 +0800

    docs:

--- plugins/gmf-plugin-snowpack/test/swcPlugin.test.ts
+++
@@ -1,8 +0,0 @@
-import { resolve } from 'path';
-import { SnowpackRunner } from '../src/SnowpackRunner';
-
-describe('SwcRunner测试', () => {
-  //
-  const runner = new SnowpackRunner();
-  runner.runAsync({ buildPath: resolve(process.cwd(), 'test/dist') });
-});
