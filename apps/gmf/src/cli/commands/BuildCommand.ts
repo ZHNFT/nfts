c507878ca2c5ecb97640ca157be39ad657ea8085
leiwenpeng <leiwenpeng1994@outlook.com>
2022-05-07 17:28:50 +0800

    chore:

--- apps/gmf/src/cli/commands/BuildCommand.ts
+++ apps/gmf/src/cli/commands/BuildCommand.ts
@@ -2,74 +2,74 @@ import { Command } from '@nfts/noddy';
 import { FlagParameter, StringParameter } from '@nfts/argparser';
 import { BuildStage } from '../../stages/BuildStage';
 
-const BUILD_LIFECYCLE_NAME = 'build';
-const BUILD_LIFECYCLE_DESC = 'Run build process';
+const NAME = 'build';
+const DESCRIPTION = 'Run build process';
 
 export interface BuildCommandInitOption {
-  stage: BuildStage;
+	stage: BuildStage;
 }
 
 export interface BuildCommandLineParameters {
-  clean: FlagParameter;
-  test: FlagParameter;
-  watch: FlagParameter;
-  tsconfig: StringParameter;
+	clean: FlagParameter;
+	test: FlagParameter;
+	watch: FlagParameter;
+	tsconfig: StringParameter;
 }
 
 export interface BuildCommandLineParametersValue {
-  clean?: boolean;
-  test?: boolean;
-  watch?: boolean;
-  tsconfig?: string;
+	clean?: boolean;
+	test?: boolean;
+	watch?: boolean;
+	tsconfig?: string;
 }
 
 export class BuildCommand extends Command implements BuildCommandLineParameters {
-  readonly stage: BuildStage;
+	readonly stage: BuildStage;
 
-  clean: FlagParameter;
-  test: FlagParameter;
-  watch: FlagParameter;
-  tsconfig: StringParameter;
+	clean: FlagParameter;
+	test: FlagParameter;
+	watch: FlagParameter;
+	tsconfig: StringParameter;
 
-  constructor({ stage }: BuildCommandInitOption) {
-    super({
-      commandName: BUILD_LIFECYCLE_NAME,
-      commandDescription: BUILD_LIFECYCLE_DESC
-    });
+	constructor({ stage }: BuildCommandInitOption) {
+		super({
+			commandName: NAME,
+			commandDescription: DESCRIPTION
+		});
 
-    this.stage = stage;
-  }
+		this.stage = stage;
+	}
 
-  onDefineParameters(): void {
-    this.clean = this.flagParameter({
-      name: '--clean',
-      summary: 'Delete the outputs of all projects.'
-    });
+	onDefineParameters(): void {
+		this.clean = this.flagParameter({
+			name: '--clean',
+			summary: 'Delete the outputs of all projects.'
+		});
 
-    this.test = this.flagParameter({
-      name: '--test',
-      summary: 'Run all test case, after build.'
-    });
+		this.test = this.flagParameter({
+			name: '--test',
+			summary: 'Run all test case, after build.'
+		});
 
-    this.watch = this.flagParameter({
-      name: '--watch',
-      summary: 'Watch input files.'
-    });
+		this.watch = this.flagParameter({
+			name: '--watch',
+			summary: 'Watch input files.'
+		});
 
-    this.tsconfig = this.stringParameter({
-      name: '--tsconfig',
-      summary: `Compile the project given the path to its configuration file, or to a folder with a 'tsconfig.json'.`
-    });
-  }
+		this.tsconfig = this.stringParameter({
+			name: '--tsconfig',
+			summary: `Compile the project given the path to its configuration file, or to a folder with a 'tsconfig.json'.`
+		});
+	}
 
-  public async onExecute(): Promise<void> {
-    const parameters: BuildCommandLineParametersValue = {
-      clean: this.clean.value,
-      test: this.test.value,
-      watch: this.watch.value,
-      tsconfig: this.tsconfig.value
-    };
+	public async onExecute(): Promise<void> {
+		const parameters: BuildCommandLineParametersValue = {
+			clean: this.clean.value,
+			test: this.test.value,
+			watch: this.watch.value,
+			tsconfig: this.tsconfig.value
+		};
 
-    await this.stage.executeAsync(parameters);
-  }
+		await this.stage.executeAsync(parameters);
+	}
 }
